4cd33865858c6c35829f8e99219cf1d8d2d3939f
Edwin Geovanny Cambisaca Barros <edwin.cambisaca@sot.gob.ec>
2020-04-11 00:14:01 -0500

    3 commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+//Settings
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//middlewares
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+
 app.get('/', (req, res) => {
     res.send('Helloworld');
 })
+
+//Static Files
+app.use(express.static(__dirname + '/public'));
+
 
 module.exports = app;
